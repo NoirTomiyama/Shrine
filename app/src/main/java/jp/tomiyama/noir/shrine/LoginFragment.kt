5f08eba828bbb57472827824da0f25982a8a409f
Ryota Tomiyama <lit.karu.noir@gmail.com>
2019-10-27 23:37:10 +0900

    add two buttons

--- app/src/main/java/jp/tomiyama/noir/shrine/LoginFragment.kt
+++ app/src/main/java/jp/tomiyama/noir/shrine/LoginFragment.kt
@@ -2,10 +2,13 @@ package jp.tomiyama.noir.shrine
 
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.shr_login_fragment.*
+import kotlinx.android.synthetic.main.shr_login_fragment.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -14,7 +17,34 @@ class LoginFragment : Fragment() {
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.shr_login_fragment, container, false)
+    val view = inflater.inflate(R.layout.shr_login_fragment, container, false)
+
+    // Set an error if the password is less than 8 characters.
+    view.next_button.setOnClickListener {
+      if (!isPasswordValid(password_edit_text.text)) {
+        password_text_input.error = getString(R.string.shr_error_password)
+      } else {
+        // Clear the error.
+        password_text_input.error = null
+      }
+    }
+
+    // Clear the error once more than 8 characters are typed.
+    view.password_edit_text.setOnKeyListener { _, _, _ ->
+      if (isPasswordValid(password_edit_text.text)) {
+        password_text_input.error = null // Clear the error
+      }
+      false
+    }
+    return view
+  }
+
+  /*
+      In reality, this will have more complex logic including, but not limited to, actual
+      authentication of the username and password.
+   */
+  private fun isPasswordValid(text: Editable?): Boolean {
+    return text != null && text.length >= 8
   }
 
 }
