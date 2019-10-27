c1e04507fb490ce668284f3f353fbccadc2682cd
Ryota Tomiyama <lit.karu.noir@gmail.com>
2019-10-27 23:19:37 +0900

    初期状態を再現

---
+++ app/src/main/java/jp/tomiyama/noir/shrine/LoginFragment.kt
@@ -0,0 +1,20 @@
+package jp.tomiyama.noir.shrine
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class LoginFragment : Fragment() {
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.shr_login_fragment, container, false)
+  }
+
+}
